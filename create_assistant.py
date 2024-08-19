bb71037cc627266c311b1be55948fcf44bee4ae4
= <56418890+pond-e@users.noreply.github.com>
2024-08-19 17:57:08 +0900

    create assistant

--- create_assistant.py
+++ create_assistant.py
@@ -1,26 +1,30 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 os.environ["OPENAI_API_KEY"]
 
 from openai import OpenAI
 client = OpenAI()
 
-
-# Fileのアップロード
-uploaded_file = client.files.create(
-    file=open("20230217_AIの進化と日本の戦略_松尾研.pdf", "rb"),
-    purpose="assistants",
-)
-file_id = uploaded_file.id
-print("file_id:", file_id)
-
+name = "Financial Analyst Assistant"
 # Assistantの作成
-my_assistant = client.beta.assistants.create(
-    name="Prompt Engineer Bot",
-    description="日本の今後のAI戦略について回答してくれるアシスタント",
-    model="gpt-4-1106-preview",
-    instructions="あなたは、現在のAIについて詳しいアシスタントです。AIの進化と日本の戦略を参考にして、回答してください。",
-    tools=[{"type": "retrieval"}, {"type": "code_interpreter"}],
-    file_ids=[file_id],
+assistant = client.beta.assistants.create(
+  name=name,
+  instructions="You are an expert financial analyst. Use you knowledge base to answer questions about audited financial statements.",
+  model="gpt-4o",
+  tools=[{"type": "file_search"}],
 )
-assistant_id = my_assistant.id
+assistant_id = assistant.id
 print("assistant_id:", assistant_id)
+
+# Write file_id and assistant_id to a CSV file
+import csv
+
+csv_filename = "assistant_info.csv"
+
+with open(csv_filename, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow([name, assistant_id])
+
+
