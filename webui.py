fab0e95efa79de667cf1eba6987395e83caed606
pond-e <haruto2266@icloud.com>
2024-08-20 12:24:50 +0900

    fix

--- webui.py
+++ webui.py
@@ -1,13 +1,18 @@
 import gradio as gr
+from create_thread import create_thread
+from show_thread_message import show_thread_message
+
+from typing import List, Tuple
 
 def handle_submit(
-    user_message: str, chat_history: List[Tuple[str, str]]
+    user_message: str, file_id: str, assistant_id: str, chat_history: List[Tuple[str, str]]
 ) -> Tuple[str, List[Tuple[str, str]]]:
     """
     gradioのchatbot機能を使うために必要な関数
     質問文とそれに対する回答のリストを返す
     """
-    bot_message = create_answer(user_message)
+    created_thread_id = create_thread(user_message, file_id)
+    bot_message = show_thread_message(created_thread_id, assistant_id)
     chat_history.append((user_message, bot_message))
     return "", chat_history, user_message # return "" で入力がクリアされる
 
@@ -19,16 +24,16 @@ with gr.Blocks() as demo:
     chatbot.show_copy_button = True
 
     regenerate_button = gr.Button("再生成")
-    attach_file = gr.Textbox(label="attach flie id")
+    file_id = gr.Textbox(label="attach flie id")
     thread_id = gr.Textbox(label="thread id")
+    assistant_id = gr.Textbox(label="assistant id")
     msg = gr.Textbox(label="質問欄")
-    password = gr.Textbox(label="パスワード")
     with gr.Row():
         submit_button = gr.Button("送信")
         clear = gr.ClearButton([msg, chatbot])
-        submit_button.click(handle_submit, inputs=[msg, chatbot], outputs=[msg, chatbot, last_user_message])
+        submit_button.click(handle_submit, inputs=[msg, file_id, assistant_id, chatbot], outputs=[msg, chatbot, last_user_message])
     
-    regenerate_button.click(handle_submit, inputs=[last_user_message, chatbot], outputs=[msg, chatbot])
+    regenerate_button.click(handle_submit, inputs=[last_user_message, file_id, assistant_id, chatbot], outputs=[msg, chatbot])
 
 
 if __name__ == "__main__":
