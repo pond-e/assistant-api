1d53ec7b81d8503326af8bb4629be50ec728b222
pond-e <haruto2266@icloud.com>
2024-08-20 11:59:08 +0900

    def

--- create_thread.py
+++ create_thread.py
@@ -5,32 +5,38 @@ load_dotenv()
 os.environ["OPENAI_API_KEY"]
 
 from openai import OpenAI
-client = OpenAI()
-
-file_id = "file-KVTYcwlNECnPGJ5Q6EuiDsgx"
- 
-# Create a thread and attach the file to the message
-thread = client.beta.threads.create(
-  messages=[
-    {
-      "role": "user",
-      "content": "What is microcluster?",
-      # Attach the new file to the message.
-      "attachments": [
-        { "file_id": file_id, "tools": [{"type": "file_search"}, {"type": "code_interpreter"}] }
-      ],
-    }
-  ]
-)
- 
-# The thread now has a vector store with that file in its tool resources.
-print(thread.tool_resources.file_search)
-
-# Write file_id and assistant_id to a CSV file
 import csv
+CSV_FILENAME = "thread_info.csv"
 
-csv_filename = "thread_info.csv"
+def create_thread(input_message: str, file_id: str) -> str:
+    client = OpenAI()
+    
+    # Create a thread and attach the file to the message
+    thread = client.beta.threads.create(
+    messages=[
+        {
+        "role": "user",
+        "content": input_message,
+        # Attach the new file to the message.
+        "attachments": [
+            { "file_id": file_id, "tools": [{"type": "file_search"}, {"type": "code_interpreter"}] }
+        ],
+        }
+    ]
+    )
+    
+    # The thread now has a vector store with that file in its tool resources.
+    print("thread.tool_resources.file_search", thread.tool_resources.file_search)
 
-with open(csv_filename, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow([thread.id])
+    # Write file_id and assistant_id to a CSV file
+    with open(CSV_FILENAME, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([thread.id])
+    
+    return thread.id
+
+
+if __name__ == "__main__":
+    input_message = "What is microcluster?"
+    file_id = "file-KVTYcwlNECnPGJ5Q6EuiDsgx"
+    create_thread(input_message, file_id)
